a81b820596fa3c1f4d9c50c1d4f94ecf0b0a4985
noisiak <kacper.bojko1@gmail.com>
2023-03-18 12:32:21 +0100

    Initial commit

---
+++ FiveM/Globbing Mode/fxmanifest.lua
@@ -0,0 +1,11 @@
+fxmanifest 'cerulean'
+game 'gta5'
+
+files {
+    'data/**/*.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
